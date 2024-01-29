67080caf63ef03825258290b5a96bea0dca90476
RayaXraya <rrayanmr2003@gmail.com>
2024-01-29 11:20:42 -0800

    l

--- printf.c
+++ printf.c
@@ -1,1 +1,35 @@
 #include "main.h"
+
+/**
+ * _printf - custom printf function
+ * @format: the format string
+ * @...: other arguments
+ *
+ * Return number of printed chars on succes
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+	int pc = 0;
+
+	va_list argu;
+
+	va_start(argu, format);
+
+	for (; *format; format++)
+	{
+		if (*format != %)
+		{
+			copy_to_buf(*format);
+			pc++;
+		}
+		else
+		{
+			format++;
+			pc += chech_specifier;
+		}
+	}
+	va_end(argu);
+	return (pc);
+}
